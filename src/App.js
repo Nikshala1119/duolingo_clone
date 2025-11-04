94db02433b0e1e9f3e69f3db087952472b426e70
Nikshala1119 <nikshala12@gmail.com>
2025-11-04 17:56:01 +0530

    phase1(st1,2)

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="app-header">
+        <h1>🦉 DuoLingo Clone</h1>
+        <p>Learn languages for free, forever.</p>
       </header>
     </div>
   );
